e425e591a2d7dde7127d7d1390bda7dfd35a08cb
apicai <apicai@users.noreply.github.com>
2023-03-17 12:56:56 +0000

    deploy: a0df2b50e3d816ad8cb8df9b62a4192fca344cf9

--- tema6/backend/files/mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/rest/ControladorRest.java
+++ tema6/backend/files/mvc/src/main/java/edu/comillas/icai/gitt/pat/spring/mvc/rest/ControladorRest.java
@@ -3,6 +3,7 @@ package edu.comillas.icai.gitt.pat.spring.mvc.rest;
 
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -24,14 +25,15 @@ public class ControladorRest {
 
     private final Map<String, ModeloContador> contadores = new HashMap<>();
 
-    @PostMapping("/contadores/{nombre}")
+    @PostMapping("/api/contadores/{nombre}")
     @ResponseStatus(HttpStatus.CREATED)
     public ModeloContador crea(@PathVariable String nombre) {
         return crea(new ModeloContador(nombre, 0L), null);
     }
 
-    @PostMapping("/contadores")
+    @PostMapping("/api/contadores")
     @ResponseStatus(HttpStatus.CREATED)
+    @PreAuthorize("hasRole('ADMIN')")
     public ModeloContador crea(@Valid @RequestBody ModeloContador contadorNuevo, BindingResult result) {
         if (result != null && result.hasErrors()) {
             throw new ExcepcionContadorIncorrecto(result);
@@ -43,20 +45,21 @@ public class ControladorRest {
         return contadorNuevo;
     }
 
-    @GetMapping("/contadores/{nombre}")
+    @GetMapping("/api/contadores/{nombre}")
     public ModeloContador contador(@PathVariable String nombre) {
         return busca(nombre);
     }
 
-    @PutMapping("/contadores/{nombre}/incrementa")
-    public ModeloContador incrementa(@PathVariable String nombre) {
+    @PutMapping("/api/contadores/{nombre}/incremento/{incremento}")
+    public ModeloContador incrementa(@PathVariable String nombre, @PathVariable Integer incremento) {
         ModeloContador contadorActual = busca(nombre);
-        ModeloContador contadorIncrementado = new ModeloContador(nombre, contadorActual.valor() + 1);
+        ModeloContador contadorIncrementado = new ModeloContador(nombre, contadorActual.valor() + incremento);
         contadores.put(nombre, contadorIncrementado);
         return contadorIncrementado;
     }
 
-    @DeleteMapping("/contadores/{nombre}")
+    @DeleteMapping("/api/contadores/{nombre}")
+    @PreAuthorize("hasRole('ADMIN')")
     public void borra(@PathVariable String nombre) {
         contadores.remove(nombre);
     }
