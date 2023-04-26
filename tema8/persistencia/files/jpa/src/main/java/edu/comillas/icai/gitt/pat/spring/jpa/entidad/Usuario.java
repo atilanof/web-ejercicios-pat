cbc016dabc5ab9e1e7abc0865e5ac8149aa270ac
apicai <apicai@users.noreply.github.com>
2023-04-26 12:56:33 +0000

    deploy: 455c66f06171bb1dfdba4b4834c6ed7ecaa9597b

--- tema8/persistencia/files/jpa/src/main/java/edu/comillas/icai/gitt/pat/spring/jpa/entidad/Usuario.java
+++ tema8/persistencia/files/jpa/src/main/java/edu/comillas/icai/gitt/pat/spring/jpa/entidad/Usuario.java
@@ -12,4 +12,6 @@ public class Usuario {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY) public Long id;
     @Column(nullable = false, unique = true) public String email;
     @Column(nullable = false) @JsonIgnore public String credenciales;
+
+    @Column(nullable = false) public String rol;
 }
--- tema8/persistencia/files/jpa/src/main/resources/static/index.js
+++ tema8/persistencia/files/jpa/src/main/resources/static/index.js
@@ -15,3 +15,60 @@ function incrementaCreaContador(contador) {
       console.error(`Error inesperado al incrementar contador "${contador}".`, error);
     });
 }
+
+let usuarioLogado;
+
+document.getElementById("entrar").onclick = function() {
+  peticionApi(`/api/usuarios/login`, null, null, document.getElementById("usuario").value, document.getElementById("clave").value)
+    .then(respuesta => respuesta.json())
+    .then(json => {
+      document.getElementById("login").hidden = true;
+      document.getElementById("estado").hidden = false;
+      if (json.email) {
+        document.getElementById("accion").value = 'Salir';
+        document.getElementById("mensaje").textContent = json.email;
+        document.getElementById("clave").value = '';
+        usuarioLogado = json;
+      } else if (json.status === 401) {
+        document.getElementById("accion").value = 'Volver';
+        document.getElementById("mensaje").textContent = 'Credenciales incorrectas';
+      } else {
+        throw json;
+      }
+    }).catch(error => {
+      document.getElementById("login").hidden = true;
+      document.getElementById("estado").hidden = false;
+      document.getElementById("accion").value = 'Reintentar';
+      document.getElementById("mensaje").textContent = 'Error inesperado';
+      console.error(`Error inesperado al hacer login`, error);
+    });
+};
+
+document.getElementById("accion").onclick = function() {
+  document.getElementById("login").hidden = false;
+  document.getElementById("estado").hidden = true;
+  peticionApi(`/api/usuarios/logout`).finally(_ => {});
+  usuarioLogado = null;
+};
+
+document.getElementById("usuario").addEventListener("keyup", function(event) {
+  if(event.key === 'Enter') {
+    event.preventDefault();
+    document.getElementById("clave").focus();
+  }
+});
+
+document.getElementById("clave").addEventListener("keyup", function(event) {
+  if(event.key === 'Enter') {
+    event.preventDefault();
+    document.getElementById("entrar").click();
+  }
+});
+
+const peticionApiSinLogout = peticionApi;
+peticionApi = function peticionApi(...args) {
+  return peticionApiSinLogout(...args).then(response => {
+    response.status === 401 && usuarioLogado && document.getElementById("accion").click();
+    return response;
+  });
+}
